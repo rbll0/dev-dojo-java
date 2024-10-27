9253bb47e56071915d492f9f5f1a63e62e09c6ef
Gustavo Rabelo <gurabelo09@gmail.com>
2024-10-26 23:13:42 -0300

    aula 22

---
+++ src/Aula22/EstruturaCondicionais03.java
@@ -0,0 +1,12 @@
+package Aula22;
+
+public class EstruturaCondicionais03 {
+    public static void main(String[] args) {
+
+        double salario = 6000;
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições";
+
+        System.out.println(resultado);
+    }
+}
